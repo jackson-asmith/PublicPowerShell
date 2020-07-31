957e72bd5e9a6dcc299c9154d2df029bec10b69d
Jackson Smith (Admin) <42005615+jackson-asmith@users.noreply.github.com>
2020-07-31 18:44:30 -0400

    Initial public commit of Get-PnpIssues.
    Get-PnpIssues scans one or more computers for plug and play driver
    device errors and status issues (degraded or unknown). Useful for
    tracking down intermittent issues with mice and keyboards, especially in
    high use environments like student labs or public access terminals.

---
+++ Get-PnpIssues.ps1
@@ -0,0 +1,34 @@
+# Scan computers for plug and play device errors and status issues (degraded or unknown).
+<#
+.DESCRIPTION
+Get-PnpIssues scans a computer or computers for plug and play device errors and status issues 
+(degrated or unknown).
+.SYNOPSIS
+Get-PnpIssues scans one or more computers for errors and degraded or unknown status. Uses PowerShell
+Remoting to get attached plug and play device status and presents data as a formatted table.
+.PARAMETER ComputerName
+The name of the computer or computers to query.
+.EXAMPLE
+Get-PnpIssues -ComputerName DESKTOP01
+
+Returns any errors or status problems for connected plug and play device issues on DESKTOP01.
+#>
+
+[CmdletBinding()]
+Param (
+    [Parameter(Mandatory = $False,
+        ValueFromPipeline = $True,
+        ValueFromPipelineByPropertyName = $True,
+        HelpMessage = "Enter the computer or computers to query.")]
+    [String[]]
+    $ComputerName = $env:COMPUTERNAME
+)
+
+Write-Verbose "Connecting to $ComputerName"
+Write-Verbose "Scanning $ComputerName for problems with connected plug and play devices"
+
+Invoke-Command -ComputerName $ComputerName -ScriptBlock {
+    Get-PnpDevice -PresentOnly -Status ERROR, DEGRADED, UNKNOWN | Format-Table -AutoSize
+}
+
+Write-Verbose "Scan complete"
